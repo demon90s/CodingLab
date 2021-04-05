060e779d3116937d509d8ae94fd88ff17fef1477
diwen.liu <demon90s@163.com>
2021-04-05 17:36:17 +0800

    update

---
+++ PHPLab/cookie_session/reset.php
@@ -0,0 +1,18 @@
+<?php
+
+// 删除 cookie
+// setcookie('font_size', '', time() - 600, '/');
+// setcookie('font_color', '', time() - 600, '/');
+
+setcookie('font_size', '');
+setcookie('font_color', '');
+
+?>
+
+<head>
+    <title>Reset Your Settings</title>
+</head>
+
+<body>
+    <p>Your settings have been reset! Click <a href="view_settings.php">here</a> to go back to the main page.</p>
+</body>
