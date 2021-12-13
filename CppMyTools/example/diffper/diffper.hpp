5c6c4365a1285b9519eb797c88b78ff41496f1ed
diwen.liu <demon90s@163.com>
2021-12-13 10:59:48 +0800

    update

--- CppMyTools/example/diffper/diffper.hpp
+++ CppMyTools/example/diffper/diffper.hpp
@@ -5,5 +5,5 @@ inline double GetDiffPer(T v1, T v2)
 {
 	// min + min * diff_per = max -> diff_per = (max - min) / min
 	v1 > v2 ? std::swap(v1, v2) : 0;
-	return (v2 - v1) * 1.0 / v1;
+	return (v2 * 1.0 - v1) / v1;
 }
