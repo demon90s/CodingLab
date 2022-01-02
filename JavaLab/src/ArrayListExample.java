d3961460cb1d37553580a0ed5b617cc73463a7fb
diwen.liu <demon90s@163.com>
2022-01-03 00:32:23 +0800

    update

--- JavaLab/src/ArrayListExample.java
+++ JavaLab/src/ArrayListExample.java
@@ -1,5 +1,47 @@
 import java.util.*;
 
+// 排序 自定义 实现接口 Comparable
+class Hero implements Comparable<Object> {
+	private String name;
+	private int capability;
+
+	Hero(String name, int capability) {
+		this.name = name;
+		this.capability = capability;
+	}
+
+	String getName() {
+		return name;
+	}
+
+	int getCapability() {
+		return capability;
+	}
+
+	@Override
+	public String toString() {
+		return new String("[" + name + "," + capability + "]");
+	}
+
+	@Override
+	public int compareTo(Object rhs) {
+		Hero rhs_hero = (Hero)rhs;
+		
+		// 如果this大于rhs，返回1，如果相等返回0,如果小于返回-1
+		if (capability != rhs_hero.capability) {
+			if (capability > rhs_hero.capability) {
+				return -1;
+			}
+			else {
+				return 1;
+			}
+		}
+		else {
+			return name.compareTo(rhs_hero.name);
+		}
+	}
+}
+
 // 测试遍历
 public class ArrayListExample {
 
@@ -65,6 +107,12 @@ public class ArrayListExample {
 		sites.add("Taobao");
 		sites.add("Weibo");
 		System.out.println(sites.get(1));			// Runnoob
+
+		try {
+			System.out.println(sites.get(100));	// 抛出异常
+		} catch (IndexOutOfBoundsException e) {
+			e.printStackTrace();
+		}
 	}
 
 	// 修改元素
@@ -104,12 +152,50 @@ public class ArrayListExample {
 		System.out.println(sites);
 	}
 
+	
+	static void testCollectionsSort2() {
+		List<Hero> hero_list = new ArrayList<Hero>();
+
+		hero_list.add(new Hero("刘备", 80));
+		hero_list.add(new Hero("关羽", 99));
+
+		Collections.sort(hero_list);
+
+		System.out.println(hero_list);
+	}
+
+	static void testCollectionsSort3() {
+		List<Hero> hero_list = new ArrayList<Hero>();
+
+		hero_list.add(new Hero("刘备", 80));
+		hero_list.add(new Hero("关羽", 99));
+
+		// 左边 优先级低返回1 优先级高返回-1
+		Comparator<Hero> cmp = (Hero lhs, Hero rhs)->{
+			if (lhs.getCapability() < rhs.getCapability()) {
+				return 1;
+			}
+			else if (lhs.getCapability() > rhs.getCapability()) {
+				return -1;
+			}
+			else {
+				return lhs.getName().compareTo(rhs.getName());
+			}
+		};
+
+		Collections.sort(hero_list, cmp);
+
+		System.out.println(hero_list);
+	}
+
 	public static void main(String[] args) {
 		//testArrayListTravel();
 		//testArrayListAdd();
 		//testArrayListGet();
 		//testArrayListSet();
 		//testArrayListRemove();
-		testCollectionsSort();
+		//testCollectionsSort();
+		//testCollectionsSort2();
+		testCollectionsSort3();
 	}
 }
---
+++ JavaLab/src/TestMain.java
@@ -0,0 +1,20 @@
+class Foo {
+
+}
+
+public class TestMain {
+    public static void main(String[] args) {
+        Foo a = new Foo();
+        
+        Foo b = a;
+        Foo c = a;
+
+        if (b == c) {
+            System.out.println("b == c");
+        }
+        else
+        {
+            System.out.println("b != c");
+        }
+    }
+}
