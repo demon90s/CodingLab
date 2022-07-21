c13b49077d3c93e389c4c419834ad177b300c1d6
diwen.liu <demon90s@163.com>
2022-07-21 21:13:28 +0800

    update

--- TMP/Actor/code/src/Actor/Actor.cpp
+++ TMP/Actor/code/src/Actor/Actor.cpp
@@ -5,17 +5,18 @@ void Actor::ProcessTask()
 {
 	// 拿出任务执行 在未执行完毕前，可以添加任务但不能注册到ActorManager(m_is_in_global控制)
 
-	std::list<ActorTask> task_queue_tmp;
-
 	{
-		std::lock_guard<std::mutex> lock(m_task_queue_mutex);
-		task_queue_tmp.swap(m_task_queue);
+		std::list<ActorTask> task_queue_tmp;
+		{
+			std::lock_guard<std::mutex> lock(m_task_queue_mutex);
+			task_queue_tmp.swap(m_task_queue);
+		}
+		for (ActorTask& actor_task : task_queue_tmp)
+		{
+			actor_task.Run();
+		}
 	}
-
-	for (ActorTask& actor_task : task_queue_tmp)
-	{
-		actor_task.Run();
-	}
+	
 
 	{
 		std::lock_guard<std::mutex> lock(m_task_queue_mutex);
